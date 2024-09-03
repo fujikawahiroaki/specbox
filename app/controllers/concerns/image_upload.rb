a7e71c6a7e6294b6d5367da9d6396b2b6a2bf41b
fujikawahiroaki <fhir0aki3@gmail.com>
2024-09-04 05:52:29 +0900

    work: 採集行の画像投稿機能の基盤を作成

---
+++ app/controllers/concerns/image_upload.rb
@@ -0,0 +1,19 @@
+module ImageUpload
+  extend ActiveSupport::Concern
+
+  def save_images(model, model_sym)
+    [ :image1, :image2, :image3, :image4, :image5 ].each do |image|
+      if params[model_sym][image].present?
+        uploaded_file = params[model_sym][image]
+        image_id = SecureRandom.uuid.to_s
+        image_path = "image-#{model.id}/#{image_id}#{File.extname(uploaded_file)}"
+        image_file_path = Rails.root.join("public", "media", image_path)
+        FileUtils.mkdir_p(File.dirname(image_file_path))
+        File.open(file_path, "wb") do |file|
+          file.write(uploaded_file.read)
+        end
+        model.update(image1: image_path)
+      end
+    end
+  end
+end
