2405c32da4ff90641152dc7c9bca64531c276ed7
fujikawahiroaki <fhir0aki3@gmail.com>
2025-01-11 16:04:58 +0900

    feat: tourのフラッシュメッセージが一定時間で消えるように変更

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,7 @@ import { Application } from "@hotwired/stimulus";
 import PreviewController from "./preview_controller";
 import LightBoxController from "./lightbox_controller";
 import ValidationController from "./validation_controller";
+import FlashController from "./flash_controller";
 
 const application = Application.start();
 
@@ -12,5 +13,6 @@ window.Stimulus = application;
 application.register("preview", PreviewController);
 application.register("lightbox", LightBoxController);
 application.register("validation", ValidationController);
+application.register("flash", FlashController);
 
 export { application };
