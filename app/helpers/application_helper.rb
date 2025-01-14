2266ec04d9d095420539fe04277f15e5c01a198c
fujikawahiroaki <fhir0aki3@gmail.com>
2025-01-15 08:06:50 +0900

    feat: 水色基調和風カラーに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,10 @@ module ApplicationHelper
   def sort_icon(field)
     if params[:sort] == field && params[:direction] == "asc"
       # 昇順の場合は ▲ を表示
-      content_tag(:span, "▲", class: "ml-2 text-blue-600")
+      content_tag(:span, "▲", class: "ml-2")
     elsif params[:sort] == field && params[:direction] == "desc"
       # 降順の場合は ▼ を表示
-      content_tag(:span, "▼", class: "ml-2 text-blue-600")
+      content_tag(:span, "▼", class: "ml-2")
     else
       # それ以外の場合は何も表示しない
       ""
